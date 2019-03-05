0c61ef2396850aa59d7f97786d989bc330a5aa56
hxnvr <hxnvr@mail.ru>
2019-03-05 19:02:37 +0300

    !

--- src/pricelist/pricelist.kt
+++ src/pricelist/pricelist.kt
@@ -6,45 +6,53 @@ package pricelist
             this.price = newPrice
         } else throw IllegalArgumentException("Неверно указана цена")
     }
+
      fun setname(newName: String){
          if (newName != "") {
              this.name = newName
          } else throw IllegalArgumentException("Неверно указано название товара")
      }
+     override fun toString():String{
+         return " Название: $name; Цена: $price"
+     }
 }
 
 class PriceList {
     private var map = mutableMapOf<Int,Product>()
-    fun add(newProductName: String, newProductCode: Int, newProductPrice: Double): MutableMap<Int,Product> {
+    fun add(newProductName: String, newProductCode: Int, newProductPrice: Double) {
         if (map.containsKey(newProductCode)) throw IllegalArgumentException("Продукт с таким кодом уже существует")
         else map[newProductCode] = Product(newProductName, newProductPrice)
-        return map
     }
-    fun clear(unCode: Int): MutableMap<Int,Product> {
+
+    fun clear(unCode: Int) {
        if (map.containsKey(unCode)){
            map.remove(unCode)
-           return map
        } else throw IllegalArgumentException("В списке нет продукта с таким кодом")
     }
+
     private fun find(code: Int): Product{
         return map[code]!!
     }
-    fun priceChange(code: Int, newPrice: Double):  MutableMap<Int,Product> {
+
+    fun priceChange(code: Int, newPrice: Double) {
         if (map.containsKey(code)) {
             find(code).setprice(newPrice)
-            return map
         }
         else throw IllegalArgumentException("В списке нет продукта с таким кодом")
     }
-    fun nameChange(code: Int, newName: String): MutableMap<Int,Product> {
+
+    fun nameChange(code: Int, newName: String) {
         if (map.containsKey(code)) {
             find(code).setname(newName)
-            return map
         }
         else throw IllegalArgumentException("В списке нет продукта с таким кодом")
     }
+
     fun purchasePrice(num: Int, code: Int): Double {
         return find(code).price * num
+    }
+    override fun toString(): String {
+        return map.toString()
     }
 }
 
