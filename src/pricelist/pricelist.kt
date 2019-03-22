2c43ee6e18a58cc16b805bb6e11c298f9d11ffd5
hxnvr <hxnvr@mail.ru>
2019-03-22 16:09:06 +0300

    done

--- src/pricelist/pricelist.kt
+++ src/pricelist/pricelist.kt
@@ -1,58 +1,67 @@
 @file:Suppress("UNUSED_PARAMETER", "unused")
 package pricelist
 
- // field
- class Product(private var name: String, var price: Double) {
-    fun newPrice(newPrice: Double) {
+ class Product(private var name: String, private var price: Double ) {
+     fun getPrice(): Double {
+         return price
+     }
+
+     fun getName(): String {
+         return name
+     }
+
+     fun newPrice(newPrice: Double) {
         if (newPrice > 0) {
             this.price = newPrice
         } else throw IllegalArgumentException("Неверно указана цена")
     }
 
      fun newName(newName: String){
-         if (newName != "") {
+         if (!newName.isBlank()) {
              this.name = newName
          } else throw IllegalArgumentException("Неверно указано название товара")
      }
 }
 
-class PriceList (var map: MutableMap<Int,Product>) {
+
+class PriceMap(private var map: MutableMap<Int,Product>) {
+
     fun add( newProductCode: Int, newProduct: Product): Boolean {
         if (map.containsKey(newProductCode)) return false
+        if (newProduct.getPrice() <= 0 || newProduct.getName().isBlank()) throw IllegalArgumentException()
         else map[newProductCode] = newProduct
         return true
     }
 
-    fun clear(unCode: Int): Boolean {
-        return if (map.containsKey(unCode)){
-            map.remove(unCode)
+    fun clear(unCode: Int): Boolean =
+        if (map.containsKey(unCode)) {map.remove(unCode)
             true
         } else false
+
+
+    private fun find(code: Int): Product {
+       if (map[code] != null) return map[code]!!
+        throw IllegalArgumentException()
     }
 
-    // !!
-    private fun find(code: Int): Product{
-        return map[code]!!
-    }
-
-    fun priceChange(code: Int, newPrice: Double): Boolean {
-        return if (map.containsKey(code)) {
+    fun priceChange(code: Int, newPrice: Double): Boolean =
+         if (map.containsKey(code)) {
             find(code).newPrice(newPrice)
             true
         } else false
-    }
 
-    fun nameChange(code: Int, newName: String): Boolean {
-        return if (map.containsKey(code)) {
+
+    fun nameChange(code: Int, newName: String): Boolean =
+         if (map.containsKey(code)) {
             find(code).newName(newName)
             true
         } else false
-    }
+
 
     fun purchasePrice(goods: List<Pair<Int, Int>>): Double {
         var sum = 0.0
         for (i in 0 until goods.size){
-            sum += find(goods[i].first).price * goods[i].second
+            sum += find(goods[i].first).getPrice() * goods[i].second
         }
         return sum
     }
--- src/test/test.kt
+++ src/test/test.kt
@@ -1,13 +1,14 @@
 package test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import pricelist.PriceList
+import pricelist.PriceMap
 import pricelist.Product
+import java.lang.IllegalArgumentException
 
 
 
 class Tests {
-    private val testMap = PriceList(
+    private val testMap = PriceMap(
         mutableMapOf(
             45 to Product("Молоко", 53.3),
             67 to Product("Печенье", 60.4), 2 to Product("Банан", 33.0)
@@ -21,7 +22,10 @@ class Tests {
         assertFalse(testMap.add(2, Product("Виноград", 343.2)))
         assertTrue(testMap.add(445, Product("Вода", 34.25)))
         assertTrue(testMap.add(8945, Product("Картошка", 14.5)))
-    }
+        assertThrows(IllegalArgumentException::class.java) { testMap.add(43, Product("", 0.0))}
+        assertThrows(IllegalArgumentException::class.java) { testMap.add(44, Product("Яблоки", 0.0))}
+        assertThrows(IllegalArgumentException::class.java) { testMap.add(4, Product("  ", 43.0))}
+        }
 
     @Test
     fun clear(){
@@ -32,12 +36,15 @@ class Tests {
         assertFalse(testMap.clear(475))
     }
 
+    //inc
     @Test
     fun priceChange() {
         assertFalse(testMap.priceChange(415, 323.22))
         assertFalse(testMap.priceChange(15, 3.2))
         assertTrue(testMap.priceChange(45,222.11))
         assertTrue(testMap.priceChange(2,12.4))
+        assertThrows(IllegalArgumentException::class.java) { testMap.priceChange(2, -3.2)}
+        assertThrows(IllegalArgumentException::class.java) { testMap.priceChange(45, 0.0)}
     }
 
     @Test
@@ -45,6 +52,8 @@ class Tests {
         assertTrue(testMap.nameChange(45, "Кефир"))
         assertTrue(testMap.nameChange(67, "Пряники"))
         assertFalse(testMap.nameChange(345,"Пиво"))
+        assertThrows(IllegalArgumentException::class.java) { testMap.nameChange(2, "")}
+        assertThrows(IllegalArgumentException::class.java) { testMap.nameChange(45, "    ")}
     }
 
     @Test
